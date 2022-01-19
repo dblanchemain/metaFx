e7b7dd9b969e04e4ff9f9502b11a142fa816d73c
dblanchemain <57360484+dblanchemain@users.noreply.github.com>
2022-01-19 23:05:22 +0100

    Add files via upload

---
+++ metaFx.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+#! /bin/sh 
+
+if [ -d $HOME/.config/metaFx ] ; then 
+
+	echo "Le dossier de travail ($HOME)/metaFx existe" 
+
+else 
+
+	mkdir $HOME/.config/metaFx
+	cp  /usr/local/share/metaFx/preferences.spc $HOME/.config/metaFx/
+
+fi 
+
+/usr/local/share/metaFx  $HOME
