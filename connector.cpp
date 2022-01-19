cd36c8ebe2c386576e10df0a46c25b86ecb9c09d
dblanchemain <57360484+dblanchemain@users.noreply.github.com>
2022-01-19 23:38:05 +0100

    Update connector.cpp

--- connector.cpp
+++ connector.cpp
@@ -2,7 +2,7 @@
  *
  * this file is part of the MetaFxe application
  *
- * Copyright 2020-2022 dominique Blanchemain
+ * Copyright 2022-2023 dominique Blanchemain
  *
  *
  * MetaSurface is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
